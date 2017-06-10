2bb2e16cd3d3d8a32de5b58904a90b554cc56250
Emily Horsman <me@emilyhorsman.com>
2017-06-10 10:52:10 -0700

    Refactor tasks import

--- photos/signal_handlers.py
+++ photos/signal_handlers.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from photos.models import PhotoFile
-from photos.tasks import download_image, read_metadata, convert_to_webp
+from photos import tasks
 
 
 @receiver(post_save, sender=PhotoFile)
@@ -20,11 +20,11 @@ def post_save_process_image(sender, instance, created, **kwargs):
     url = os.environ.get('BASE_URL', '')
     name, _ = os.path.splitext(os.path.basename(instance.file.name))
     chain(
-        download_image.s(
+        tasks.download_image.s(
             instance.pk,
             urljoin(url, instance.file.url),
         ),
 
-        read_metadata.s(instance.pk),
-        convert_to_webp.s(name, instance.pk),
+        tasks.read_metadata.s(instance.pk),
+        tasks.convert_to_webp.s(name, instance.pk),
     )()
